7aca3a5b3fa4691616ba1210d67883a2ac7852d3
Jaslev <julia.ruppert@ingka.ikea.com>
2021-11-03 13:36:30 +0100

    comments

--- calculation.js
+++ calculation.js
@@ -1,5 +1,5 @@
 function calculate() {
-    //alle Input boxen
+    //all input fields
     let arrivalHour = parseInt(document.getElementById("arrivalHour").value);
     let arrivalMinute = parseInt(document.getElementById("arrivalMinute").value);
     let stayHour = parseInt(document.getElementById("stayHour").value);
@@ -9,23 +9,28 @@ function calculate() {
     let leaveHour = parseInt(document.getElementById("leaveHour").value);
     let leaveMinute = parseInt(document.getElementById("leaveMinute").value);
     
-    //alle Checkboxen
+    //all checkboxes
     const overtimeCheck = document.getElementById("overtimeUse");
     const leaveCheck = document.getElementById("leaveUse");
 
+    // global calculation to calculate when to times are more than 60 min.
     if (arrivalMinute + stayMinute > 59) {
         stayHour = stayHour + 1;
         stayMinute = stayMinute - 60;
+
     }
     if (overtimeCheck.checked & leaveCheck.checked) {
+        // calculates the new overtime considering all values
         console.log(overtimeCheck.checked);
         console.log(leaveCheck.checked);
     }
     else if (overtimeCheck.checked) {
+        // calculates the time you can you go considering overtime
         console.log(overtimeCheck.checked);
         console.log(leaveCheck.checked);
     }
     else if (leaveCheck.checked) {
+        // calculates the overtime based on arrival and leave time
         if((leaveMinute-(stayMinute+arrivalMinute))<0) {
             leaveHour = leaveHour - 1;
             leaveMinute = leaveMinute + 60;
@@ -34,6 +39,7 @@ function calculate() {
         document.getElementById("resultMinute").innerHTML = leaveMinute - (stayMinute+arrivalMinute);
     }
     else {
+        // calculates the time you can go withoout any overtime usage (8:27h)
         document.getElementById("resultHour").innerHTML = stayHour + arrivalHour;
         document.getElementById("resultMinute").innerHTML = stayMinute + arrivalMinute;
     }
